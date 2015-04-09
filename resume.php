476624ce856bf5631defc5ed4813155445907736
lillyferrilly <lthomps4@uvm.edu>
2015-04-09 19:56:46 -0400

    main page, and three stylesheets

---
+++ resume.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Lilly Thompson</title>
+ 
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
+
+	<meta name="description" content="I am currently pursuing a Bachelor of Arts in Computer Science at the University of Vermont.
+                                        My interests lie mainly in Web and Software Development." >
+
+	<meta name="author" content="Lilly Thompson" >
+
+        <link rel="stylesheet" type="text/css" href="resume.css" title="Professional" />
+        <link rel="alternate stylesheet" type="text/css" href="resume1.css" title="Creative" />
+        <link rel="alternate stylesheet" type="text/css" href="print.css" title="Print" />
+        <link href='https://fonts.googleapis.com/css?family=Glegoo' rel='stylesheet' type='text/css'>
+
+
+<div class="wrapper1">	
+<div id="hcard-Lilly-Thompson" class="vcard">
+ 
+<h1 id="name"><a target="_blank" class="urlfn" href="https://www.linkedin.com/pub/lillian-thompson/b6/5b7/368">Lilly Thompson</a></h1>
+  
+<!-- Address Info -->
+<div class="adr">
+    <span class="street-address">482 Quaker Street</span>, 
+    <span class="locality">North Ferrisburgh</span>,  
+    <span class="region">Vermont</span>, 
+    <span class="postal-code">05473</span>
+    <span class="country-name">USA</span>
+</div>
+
+<!-- Email Info -->
+<div class="email">
+    <span class="type">Email: </span>
+			
+    <span class="value"><a class="email1" href="mailto:lthomps4@uvm.edu">lthomps4@uvm.edu</a></span>
+</div>
+
+<!-- Phone Info -->
+<div class="tel">
+    <span class="type">Cell: </span>
+    <span class="value">1.802.238.5880</span>
+</div>
+
+<div class="tel">
+    <span class="type">Home: </span>
+    <span class="value">1.802.425.3640</span>
+</div>
+
+</div>  <!-- ends div class vcard -->
+</div>
+
+<!-- Education section -->
+<div class="wrapper2">
+<div id="education">
+<h2>Education</h2>
+
+<ul>
+    <li class="school" id="uvm">
+    <span class="dates">2010 - Present</span>
+    <a target="_blank" href="http://www.uvm.edu" class="institution">University of Vermont</a>, 
+    <a target="_blank" href="http://maps.google.com/maps?q=+colchester+ave%2C+Burlington%2C+VT+05401%2C+United+States&amp;l=explore&amp;utm_campaign=en&amp;utm_medium=ha&amp;utm_source=en-ha-na-us-gns-lt&amp;utm_term=searchbox&amp;submit.x=116&amp;submit.y=23&amp;submit=Explore+this+place" 
+class="location">Burlington, VT</a>
+
+    <span class="MajorDegree">BA in Computer Science</span>
+    <span class="MinorDegree">Anthropology</span>
+
+    <!-- List the most important courses for your major that you have taken (or will take). This gives you some common ground when interviewing and something to talk about. list the NAME not the number. If there is a link for your course put it in but be sure the link will always
+    be available. -->
+    <h3 class="subtitle">Course of Study </h3>
+    <ul class="courses major">
+        <li>Introduction to Web Design</li>
+        <li>Advanced Web Design</li>
+        <li>Computer Networking</li>
+        <li>Software Engineering</li>
+        <li>Operating Systems</li>
+    </ul><br>
+
+
+    <ul class="courses minor">					
+        <li>Cultural Anthropology</li>
+        <li>Death, Dying, and Disposal</li>
+        <li>Food, Culture, & Politics</li>
+        <li>North American Indians</li>
+    </ul>
+    </li> <!-- end school  -->
+
+</ul>
+</div> <!-- end education division -->
+
+
+<!-- Work History -->
+<div id="experience">
+<h2>Work Experience</h2>
+
+<!-- Begin job -->
+<ul>
+    <li class="job">
+        <span class="dates">2012 - Present</span>
+        <a target="_blank" href="http://www.poolworld.com" class="company">Pool World</a>,  
+        <a target="_blank" href="https://www.google.com/maps/place/Pool+World+Inc/@44.451719,-73.219248,17z/data=!3m1!4b1!4m2!3m1!1s0x4cca7ba16f8bae17:0x948caca2ea58766a" class="location">Burlington, VT</a>
+
+        <span class="jobtitle">Sales Associate</span>
+
+        <p class="description">Analyze customer's pool/hot tub water; aid customers in understanding basic water chemistry; locate chemicals, products, and parts for customers; cash out customers; and deliver hot tub, swimming pool, and pool table presentations to potential buyers.</p>
+    </li><!-- end of this job -->
+    <!-- Begin job -->
+    <li class="job">
+        <span class="dates">Summer 2010</span>
+        <a class="company1">The Main Scoop</a>,  
+        <a target="_blank" href="https://www.google.com/maps/place/61+Main+St,+Vergennes,+VT+05491/@44.171463,-73.247711,17z/data=!3m1!4b1!4m2!3m1!1s0x4cca9d785b35d3e1:0xf66db14ffe56d951" class="location">Vergennes, VT</a>
+
+        <span class="jobtitle">Server</span>
+
+        <p class="description">Scooped ice cream and served customers; operated cash register; and cleaned all counters, machines, dishes, and floor.</p>
+    </li><!-- end of this job -->
+    <!-- Begin job -->
+    <li class="job">
+        <span class="dates">Winters 2009 - 2013</span>
+        <a target="_blank" href="http://www.dakinfarm.com/" class="company">Dakin Farm</a>,  
+        <a target="_blank" href="https://www.google.com/maps/place/Dakin+Farm/@44.24259,-73.230435,11z/data=!4m5!1m2!2m1!1sdakin+farm+vt!3m1!1s0x4cca8259f5d4f225:0x6bf24add2fe04808" class="location">North Ferrisburgh, VT</a>
+
+        <span class="jobtitle">Holiday Packer</span>
+
+        <p class="description">Confirmed all products ordered were present, and packed customer's gift box for shipment.</p>
+    </li><!-- end of this job -->
+
+</ul>		
+</div> <!-- end of your Experience-->
+
+<!-- Skill set section -->
+<div id="skills">
+<h2>Skills</h2>
+    <ol>
+        <li class="int">Proficient in Java, HTML, CSS, and MATLAB</li>
+        <li class="int">Sufficient in C, PHP, JavaScript, and MySQL</li>
+        <li class="int">Software Development</li>
+        <li class="int">Agile Development</li>
+        <li class="int">Leadership</li>
+        <li class="int">Computer Networking</li>
+        <li class="int">Customer Service</li>
+    </ol>
+</div> <!-- ends skill sets -->
+
+<!-- Interests section -->
+<div id="interests">
+<h2>Activities & Interests</h2>
+	<ol>
+            <li class="int">Planning and facilitating Boy Scout's Digital Technology Merit Badge event, May 2015</li>
+            <li class="int">Participated in State Street/UVM Agile CodeFest, March 2015</li>
+            <li class="int">Team Lead, Software Engineering final project, Spring 2015</li>
+            <li class="int">Photography, Reading, Video Games, Hiking, Cats, Cooking, and People</li>
+        </ol>
+</div> <!-- ends interests -->
+
+</div>
+
+    <div class="wrapper3">
+        <p>Design by Lilly Thompson</p>
+    </div>
+</body>
+
+</html>+
